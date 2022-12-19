e6892ffadf60b02cd023dca336271e9e8d217472
Benedict Lau <b@deprecated.systems>
2022-12-18 23:14:05 -0500

    Update asset processing

--- internal/asset_processing/generate_assets.py
+++ internal/asset_processing/generate_assets.py
@@ -19,6 +19,7 @@ bitcoin_node_url = os.environ.get("BITCOIN_NODE_URL")
 c2patool_path = os.environ.get("C2PATOOL_PATH")
 
 path_c2patool = os.path.abspath(c2patool_path)
+path_asset_info_ext = "asset_info_ext.json"
 path_default = "assets/default/"
 path_redaction_photoshop = "assets/redaction_photoshop/"
 path_redaction_zk = "assets/redaction_zk/"
@@ -58,7 +59,7 @@ class ArchiveManifests():
 def _get_index_by_label(c2pa, label):
     return [i for i, o in enumerate(c2pa["assertions"]) if o["label"] == label][0]
 
-def _generate_c2pa_1_src_from_archive(archive_manifests, path_archive, path_c2pa_1_template, path_c2pa_1_src):
+def _generate_c2pa_1_src_from_archive(archive_manifests, asset_info_ext, path_archive, path_c2pa_1_template, path_c2pa_1_src):
     with open(path_c2pa_1_template, "r") as c2pa_1_template:
         c2pa_1 = json.load(c2pa_1_template)
 
@@ -67,7 +68,7 @@ def _generate_c2pa_1_src_from_archive(archive_manifests, path_archive, path_c2pa
             content_hash = None
             ext = None
 
-            # Prepare c2pa manifest for injection from content metadata
+            # Prepare c2pa manifest for injection
             for filename in zf.namelist():
                 if filename.endswith("-meta-content.json"):
                     f = zf.read(filename)
@@ -77,6 +78,12 @@ def _generate_c2pa_1_src_from_archive(archive_manifests, path_archive, path_c2pa
                     source_id = content_metadata.get("sourceId").get("value")
                     if source_id is None:
                         raise Exception("Missing sourceId")
+                    info_ext = asset_info_ext.get(source_id)
+                    if info_ext is None:
+                        raise Exception(f"Missing assetInfoExt for {source_id}")
+
+                    # Insert claim generator
+                    c2pa_1["claim_generator"] = info_ext.get("claimGenerator")
 
                     # Insert authorship information
                     m = _get_index_by_label(c2pa_1, "stds.schema-org.CreativeWork")
@@ -84,19 +91,27 @@ def _generate_c2pa_1_src_from_archive(archive_manifests, path_archive, path_c2pa
                     c2pa_1["assertions"][m]["data"]["author"][0]["identifier"] = content_metadata.get("author").get("identifier")
                     c2pa_1["assertions"][m]["data"]["author"][0]["name"] = content_metadata.get("author").get("name")
 
-                    # Insert c2pa actions
+                    # Insert c2pa.created actions
                     m = _get_index_by_label(c2pa_1, "c2pa.actions")
                     n = [i for i, o in enumerate(c2pa_1["assertions"][m]["data"]["actions"]) if o["action"] == "c2pa.created"][0]
-                    c2pa_1["assertions"][m]["data"]["actions"][n]["when"] = content_metadata.get("dateCreated")
-                    now = datetime.utcnow().replace(microsecond=0).isoformat() + "Z"
-                    tmp = [i for i, o in enumerate(c2pa_1["assertions"][m]["data"]["actions"]) if o["action"] == "c2pa.converted"]
-                    if tmp:
-                        n = tmp[0]
-                        c2pa_1["assertions"][m]["data"]["actions"][n]["when"] = now
-                    tmp = [i for i, o in enumerate(c2pa_1["assertions"][m]["data"]["actions"]) if o["action"] == "c2pa.resized"]
-                    if tmp:
-                        n = tmp[0]
-                        c2pa_1["assertions"][m]["data"]["actions"][n]["when"] = now
+                    c2pa_1["assertions"][m]["data"]["actions"][n]["when"] = info_ext.get("captureDate")
+
+                    # Insert c2pa.converted and c2pa.resized actions for ZK redaction assets
+                    if info_ext.get("redaction") is "ZK":
+                        now = datetime.utcnow().replace(microsecond=0).isoformat() + "Z"
+                        c2pa_1["assertions"][m]["data"]["actions"].append({ "action": "c2pa.converted", "when": now })
+                        c2pa_1["assertions"][m]["data"]["actions"].append({ "action": "c2pa.resized", "when": now })
+
+                    # Insert iptc metadata
+                    m = _get_index_by_label(c2pa_1, "stds.iptc.photo-metadata")
+                    c2pa_1["assertions"][m]["data"]["dc:creator"][0] = info_ext.get("capturedBy")
+                    c2pa_1["assertions"][m]["data"]["dc:description"] = info_ext.get("captionText")
+                    location = {}
+                    if info_ext.get("countryCode"): location["Iptc4xmpExt:CountryCode"] = info_ext.get("countryCode")
+                    if info_ext.get("countryName"): location["Iptc4xmpExt:CountryName"] = info_ext.get("countryName")
+                    if info_ext.get("provinceState"): location["Iptc4xmpExt:ProvinceState"] = info_ext.get("provinceState")
+                    if info_ext.get("city"): location["Iptc4xmpExt:City"] = info_ext.get("city")
+                    c2pa_1["assertions"][m]["data"]["Iptc4xmpExt:LocationCreated"] = location
 
                     # Insert root of trust signatures
                     m = _get_index_by_label(c2pa_1, "org.starlinglab.integrity")
@@ -117,6 +132,8 @@ def _generate_c2pa_1_src_from_archive(archive_manifests, path_archive, path_c2pa
                         if sig.get("custom"): x["starling:custom"] = sig.get("custom")
                         c2pa_1["assertions"][m]["data"]["starling:signatures"].append(x)
 
+                    # TODO: Insert root of trust signatures of the source asset (only for images derived from a source such as WACZ or ProofMode)
+
                     # Insert archive manifests
                     c2pa_1["assertions"][m]["data"]["starling:archives"] = []
                     manifest = archive_manifests.get_manifest(content_hash)
@@ -136,8 +153,8 @@ def _generate_c2pa_1_src_from_archive(archive_manifests, path_archive, path_c2pa
             for filename in zf.namelist():
                 if filename.endswith(f"{content_hash}.{ext}.authsign"):
                     f = zf.read(filename)
-                    sig_str = f.decode('unicode_escape').replace('"{', "{").replace('}"', "}") # authsign signatures are encoded as strings due to a backend bug
-                    sig = json.loads(sig_str)
+                    # sig = json.load(f)
+                    sig = json.loads(f.decode('unicode_escape').replace('"{', "{").replace('}"', "}")) # authsign signatures are encoded as strings due to a backend bug
                     sig_provider = sig.get("software")
                     sig_algo = "ecdsa-certificate-sig"
                     x = {
@@ -147,6 +164,8 @@ def _generate_c2pa_1_src_from_archive(archive_manifests, path_archive, path_c2pa
                     }
                     m = _get_index_by_label(c2pa_1, "org.starlinglab.integrity")
                     c2pa_1["assertions"][m]["data"]["starling:signatures"].append(x)
+
+            # TODO: Insert authsign signatures of the source asset (only for images derived from a source such as WACZ or ProofMode)
 
             # Insert opentimestamps registration record
             for filename in zf.namelist():
@@ -174,7 +193,7 @@ def _generate_c2pa_1_src_from_archive(archive_manifests, path_archive, path_c2pa
             with open(os.path.join(path_c2pa_1_src, f"{source_id}.json"), "w") as man:
                 json.dump(c2pa_1, man)
 
-def _generate_c2pa_1_out_from_src(archive_manifests, path_assets):
+def _generate_c2pa_1_out_from_src(archive_manifests, asset_info_ext, path_assets):
     path_archives = os.path.join(path_assets, "archives")
     path_c2pa_1_src = os.path.join(path_assets, "c2pa_1_src")
     path_c2pa_1_out = os.path.join(path_assets, "c2pa_1_out")
@@ -185,10 +204,10 @@ def _generate_c2pa_1_out_from_src(archive_manifests, path_assets):
     for archive in os.listdir(path_archives):
         if archive.endswith(".zip"):
             path_archive = os.path.join(path_archives, archive)
-            _generate_c2pa_1_src_from_archive(archive_manifests, path_archive, path_c2pa_1_template, path_c2pa_1_src)
+            _generate_c2pa_1_src_from_archive(archive_manifests, asset_info_ext, path_archive, path_c2pa_1_template, path_c2pa_1_src)
 
             # Uncomment to process just a single asset in the directory
-            #break
+            break
 
     # Generate c2pa injected assets in path_c2pa_1_out
     for filename in os.listdir(path_c2pa_1_src):
@@ -210,7 +229,7 @@ def _generate_c2pa_1_out_from_src(archive_manifests, path_assets):
             with open(path_out_man, "w") as f:
                 p = subprocess.run([f"{path_c2patool}", f"{path_out}", "--detailed"], stdout=f)
 
-def _generate_layer3_out_from_src(archive_manifests, path_assets):
+def _generate_layer3_out_from_src(archive_manifests, asset_info_ext, path_assets):
     path_c2pa_1_src = os.path.join(path_assets, "c2pa_1_src")
     path_layer3_out = os.path.join(path_assets, "layer3_out")
     path_layer3_template = os.path.join(path_assets, "layer3_template.json")
@@ -229,28 +248,39 @@ def _generate_layer3_out_from_src(archive_manifests, path_assets):
                     # Select archive manifest of main asset
                     m = _get_index_by_label(asset_info, "org.starlinglab.integrity")
                     n = [i for i, o in enumerate(asset_info["assertions"][m]["data"]["starling:archives"]) if o["sourceId"]["key"] == "data_id" and o["sourceId"]["value"] == source_id][0]
-                    asset_archive_manifest = asset_info["assertions"][m]["data"]["starling:archives"][n]
+                    asset_archive_manifest = asset_info["assertions"][m]["data"]["starling:archives"][n] # TODO: get this from file
                     # print(asset_archive_manifest)
+                    info_ext = asset_info_ext.get(source_id)
 
-                    # Get image information
+                    # TODO: Get image information
                     with Image.open(path_asset_img) as asset_img:
                         w, h = asset_img.size
                         asset_dimensions = f"{w} x {h}"
                         asset_format = asset_img.get_format_mimetype()
 
-                    # Insert main asset information 
+                    # Insert main asset information
                     layer3 = json.load(layer3_template)
-                    layer3["description"] = "# TODO"
+                    layer3["description"] = info_ext.get("description")
                     layer3["assetCid"] = asset_archive_manifest["archiveEncrypted"]["cid"]
-                    layer3["assetDetails"]["dimensions"] = asset_dimensions
-                    layer3["assetDetails"]["format"] = asset_format
+                    layer3["caption"]["captionText"] = info_ext.get("captionText")
+                    layer3["caption"]["originalUrl"] = info_ext.get("originalUrl")
+
+                    layer3["assetDetails"]["dimensions"] = asset_dimensions # TODO
+                    layer3["assetDetails"]["format"] = asset_format # TODO
                     layer3["c2pa"]["assetFile"] = f"{source_id}.{ext}"
                     layer3["c2pa"]["manifestFile"] = f"{source_id}-manifest.json"
-                    m = _get_index_by_label(asset_info, "c2pa.actions")
-                    n = [i for i, o in enumerate(asset_info["assertions"][m]["data"]["actions"]) if o["action"] == "c2pa.created"][0]
-                    layer3["captureDate"] = asset_info["assertions"][m]["data"]["actions"][n]["when"]
-                    layer3["capturedBy"] = "# TODO"
-                    layer3["location"] = "# TODO"
+
+                    layer3["captureDate"] = info_ext.get("captureDate")
+                    layer3["capturedBy"] = info_ext.get("capturedBy")
+
+                    location = info_ext.get("countryName")
+                    if info_ext.get("city"):
+                        location = f"{info_ext.get('city')}, {info_ext.get('provinceState')}, {info_ext.get('countryName')}"
+                    elif info_ext.get("provinceState"):
+                        location = f"{info_ext.get('provinceState')}, {info_ext.get('countryName')}"
+                    layer3["location"] = location
+
+                    # Insert registration records
                     registration_records = asset_archive_manifest["registrationRecords"]
                     layer3["registrationRecords"]["openTimestamps"]["sha256"] = registration_records["openTimestamps"]["sha256"]
                     layer3["registrationRecords"]["openTimestamps"]["block"] = registration_records["openTimestamps"]["block"]
@@ -258,13 +288,46 @@ def _generate_layer3_out_from_src(archive_manifests, path_assets):
                     layer3["registrationRecords"]["numbersProtocolAvalanche"]["tx"] = registration_records["numbersProtocol"]["avalancheTxHash"]
                     layer3["registrationRecords"]["iscn"]["iscnId"] = registration_records["iscn"]["iscnId"]
                     layer3["registrationRecords"]["iscn"]["tx"] = registration_records["iscn"]["txHash"]
-                    layer3["storageRecords"]["ipfs"]["cid"] = "# TODO"
-                    layer3["storageRecords"]["filecoin"]["pieceCid"] = "# TODO"
-                    layer3["storageRecords"]["storj"]["path"] = "# TODO"
 
-                    # Insert attestation assets information
+                    # Insert storage records
+                    layer3["storageRecords"]["ipfs"]["cid"] = info_ext.get("ipfsCid")
+                    layer3["storageRecords"]["filecoin"]["pieceCid"] = info_ext.get("filecoinPieceCid")
+                    layer3["storageRecords"]["storj"]["path"] = info_ext.get("storjPath")
+
+                    # Insert c2pa manifests
+                    layer3["verifiedBy"] = []
+                    if info_ext.get("c2paManifest1"):
+                        c2pa_1 = {
+                            "name": info_ext.get("c2paManifest1"),
+                            "value": "Starling Lab Bijeljina Investigation",
+                            "file": "starling-lab-bijeljina-investigation.cert.pem"
+                        }
+                        layer3["verifiedBy"].append(c2pa_1)
+                    if info_ext.get("c2paManifest2"):
+                        if info_ext.get("c2paManifest2") is "ZK":
+                            c2pa_2 = {
+                                "name": info_ext.get("c2paManifest2"),
+                                "value": "Starling Lab Bijeljina Investigation",
+                                "file": "starling-lab-bijeljina-investigation.cert.pem"
+                            }
+                        else:
+                            c2pa_2 = {
+                                "name": info_ext.get("c2paManifest2"),
+                                "value": "Adobe Photoshop",
+                                "file": "adobe-photoshop.cert.pem"
+                            } 
+                        layer3["verifiedBy"].append(c2pa_2)
+                    if info_ext.get("c2paManifest3"):
+                        c2pa_3 = {
+                            "name": info_ext.get("c2paManifest3"),
+                            "value": "Adobe Photoshop",
+                            "file": "adobe-photoshop.cert.pem"
+                        }
+                        layer3["verifiedBy"].append(c2pa_3)
+
+                    # TODO: Insert attestation assets information
                     layer3["attestations"] = []
-                    # TODO: layer3["attestations"].append(attestation)
+                    # layer3["attestations"].append(attestation)
 
                     # print(json.dumps(layer3, indent=2))
                     with open(os.path.join(path_layer3_out, f"{source_id}.json"), "w") as man:
@@ -278,9 +341,11 @@ def _generate_layer3_out_from_src(archive_manifests, path_assets):
 # Index archive manifests from path_archive_manifests
 path_archive_manifests = os.path.join(path_default, "archive_manifests")
 archive_manifests = ArchiveManifests(path_archive_manifests)
+with open(path_asset_info_ext, "r") as f:
+    asset_info_ext = json.load(f)["assetInfoExt"]
 
-_generate_c2pa_1_out_from_src(archive_manifests, path_default)
-_generate_layer3_out_from_src(archive_manifests, path_default)
+_generate_c2pa_1_out_from_src(archive_manifests, asset_info_ext, path_default)
+_generate_layer3_out_from_src(archive_manifests, asset_info_ext, path_default)
 
 # _generate_c2pa_1_out_from_src(archive_manifests, path_redaction_photoshop)
 # _generate_c2pa_1_out_from_src(archive_manifests, path_redaction_zk)
